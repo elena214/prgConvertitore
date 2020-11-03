e6d50fc8437305671ac7751a0bc7063d2f645eb6
elena214 <elebe2104@gmail.com>
2020-11-03 11:46:08 +0100

    convertitore aggiornato

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Covertitore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double resto;
+            string numeroconvertito = "";
+            Console.WriteLine("Inserisci un numero decimale:");
+            int numerodecimale = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci un divisore:");
+            int divisore = Convert.ToInt32(Console.ReadLine());
+            while (numerodecimale > 0)
+            {
+                resto = numerodecimale % divisore;
+                numerodecimale = numerodecimale / 2;
+                numeroconvertito = Convert.ToString(resto) + numeroconvertito;
+            }
+            Console.WriteLine("{0}", numeroconvertito);
+            Console.ReadLine();
+        }
+    }
+}
--- prgConvertitore/Program.cs
+++ prgConvertitore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlTypes;
 
 namespace prgConvertitore
 {
@@ -6,7 +7,48 @@ namespace prgConvertitore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double resto;
+            string numeroconvertito = "";
+            Console.WriteLine("Inserisci un numero decimale");
+            int numerodecimale = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci un divisore");
+            int divisore = Convert.ToInt32(Console.ReadLine());
+            while (numerodecimale > 0) 
+            {
+                resto = numerodecimale % divisore;
+                numerodecimale = numerodecimale / divisore;
+                
+                if(resto == 10)
+                {
+                    numeroconvertito = "A" + numeroconvertito;
+                }
+                else if(resto == 11)
+                {
+                    numeroconvertito = "B" + numeroconvertito;
+                }
+                else if(resto == 12)
+                {
+                    numeroconvertito = "C" + numeroconvertito;
+                }
+                else if(resto == 13)
+                {
+                    numeroconvertito = "D" + numeroconvertito;
+                }
+                else if (resto == 14)
+                {
+                    numeroconvertito = "E" + numeroconvertito;
+                }
+                else if (resto == 15)
+                {
+                    numeroconvertito = "F" + numeroconvertito;
+                }
+                else 
+                {
+                    numeroconvertito = Convert.ToString(resto) + numeroconvertito;
+                }
+            }
+            Console.WriteLine("{0}", numeroconvertito );
+            Console.ReadLine();
         }
     }
 }
